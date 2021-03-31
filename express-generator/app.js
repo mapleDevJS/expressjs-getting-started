f3b599eb374be5d6845ee040296dc7062e1a3eff
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-03-31 18:23:47 -0400

    add basic routes

--- express-generator/app.js
+++ express-generator/app.js
@@ -38,4 +38,20 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
 module.exports = app;
