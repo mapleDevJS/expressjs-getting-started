fe9d2a0594d246ed87c0a9c289dbebe2476c65e6
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-04-01 16:32:13 -0400

    change port to constant

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 1338;
+const PORT = 1338;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
 })
+
